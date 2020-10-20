3ef44c778a38d7b20026c35119b32d9b2840ee1f
StevenLiang <skt1piglelt@gmail.com>
2020-10-20 22:30:36 +0800

    最近工作忙初始化一下准备继续开发

---
+++ src/views/Money.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout class-prefix="layout">
+    <NumberPad/>
+    <Types/>
+    <Notes/>
+    <Tags :data-source.sync="tags"/>
+  </Layout>
+</template>
+
+<script>
+import NumberPad from '@/components/Money/NumberPad.vue';
+import Types from '@/components/Money/Types.vue';
+import Notes from '@/components/Money/Notes.vue';
+import Tags from '@/components/Money/Tags.vue';
+
+export default {
+  name: 'Money',
+  components: {Tags, Notes, Types, NumberPad},
+  data() {
+    return {
+      tags: ['衣', '食', '住', '行', '吃', '喝', '嫖', '赌']
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
+</style>
+
